31560618042cc95ec2629351440a076feb4ee88e
Toqaa Ahmed <toqaaahmedm-ux.com>
2026-08-17 12:07:35 +0300

    chore: remove test artifacts accidentally committed with ClassSession changes

--- check-question.js
+++
@@ -1,7 +0,0 @@
-﻿const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-(async () => {
-  const bad = await prisma.question.findMany({ where: { correctIndex: null } });
-  console.log('Found questions with NULL correctIndex:', JSON.stringify(bad, null, 2));
-  await prisma.$disconnect();
-})();
